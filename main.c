e0ee6c4e73736b7f18c4568f0bb4a57307a22044
Fabien Grasset <fabien@Fabiens-MacBook-Pro.local>
2022-10-17 16:55:04 +0200

    First changes made and added basic functions struct

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Libc                                               :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fabien <fabien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/07 18:02:35 by fabien            #+#    #+#             */
-/*   Updated: 2022/10/07 18:02:48 by fabien           ###   ########.fr       */
+/*   Created: 2022/10/12 14:09:38 by fabien            #+#    #+#             */
+/*   Updated: 2022/10/17 16:52:13 by fabien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+int main(int ac, char **av)
+{
+    (void) ac;
+
+    printf("Test de memset --- Expected result ()");
+    char str[20];
+    memset(str, '*', 9);
+    printf("result of memset: ", str);
+
+
+    printf("Test de bzero --- Expected result ()");
+
+}
