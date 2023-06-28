2b0a92f639bc8f47d0e6bc94f4aff058b83d510b
Fabien Grasset <fabien.grasset.99@gmail.com>
2023-06-28 16:54:16 +0200

    working on the exam

---
+++ rank03/printf/ft_printf.c
@@ -0,0 +1,65 @@
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+
+void	ft_putstr(char *input, int	*len)
+{
+	int	i;
+
+	i = -1;
+	while (input[++i])
+		len += write(1, &input[i], 1);
+}
+
+void	ft_putnbrs(int nbr, int base, int *len)
+{
+	char	*hexa = "0123456789abcdef";
+
+	if (nbr < 0)
+	{
+		len += write(1, "-", 1);
+		nbr *= -1;
+	}
+	if (nbr > base)
+		ft_putnbrs((nbr / base), base, len);
+	len += write(1, &hexa[nbr % base], 1);
+}
+
+int	ft_printf(char *input, ...)
+{
+	va_list	arg;
+	int		len;
+	int		i;
+
+	va_start(arg, input);
+	len = 0;
+	if (input == NULL)
+		return (0);
+	i = -1;
+	while (input[++i])
+	{
+		if (input[i] == '%')
+		{
+			i++;
+			if (input[i] == 's')
+				ft_putstr(va_arg(arg, char *), &len);
+			else if (input[i] == 'd')
+				ft_putnbrs((long long int)va_arg(arg, int), 10, &len);
+			else if (input[i] == 'x')
+				ft_putnbrs((long long int)va_arg(arg, unsigned int), 16, &len);
+		}
+		else
+			len += write(1, &input[i], 1);
+	}
+	va_end(arg);
+	return (len);
+}
+
+int	main()
+{
+	int	nb = 10;
+
+	ft_printf("salut les top %d\n", nb);
+	return (0);
+}
