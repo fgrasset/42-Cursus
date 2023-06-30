3f3e5ea9b06571fdb19d53b090a0488b6a319fe1
Fabien Grasset <fabien.grasset.99@gmail.com>
2023-06-30 08:31:24 +0200

    working on exam rank03

---
+++ rank03/ft_printf/ft_printf.c
@@ -0,0 +1,62 @@
+# include <unistd.h>
+# include <stdarg.h>
+
+void	putstr(char *input, int *len);
+void	putnbr(long long int nbr, int base, int *len);
+
+int	ft_printf(char *input, ...)
+{
+	va_list	arg;
+	int		len = 0;
+	int		i = -1;
+
+	va_start(arg, input);
+	if (!input)
+		return (0);
+	while (input[++i])
+	{
+		if (input[i] == '%' && (input[i + 1] == 's' | input[i + 1] == 'd' | input[i + 1] == 'x'))
+		{
+			i++;
+			if (input[i] == 's')
+				putstr(va_arg(arg, char *), &len);
+			else if (input[i] == 'd')
+				putnbr((long long int)va_arg(arg, int), 10, &len);
+			else if (input[i] == 'x')
+				putnbr((long long int)va_arg(arg, unsigned int), 16, &len);
+		}
+		else
+			len += write(1, &input[i], 1);
+	}
+	va_end(arg);
+	return (len);
+}
+
+void	putstr(char *input, int *len)
+{
+	int	i = -1;
+
+	if (!input)
+	{
+		*len += write(1, "(null)", 6);
+		return ;
+	}
+	while (input[++i])
+	{
+		*len += write(1, &input[i], 1);
+	}
+}
+
+void	putnbr(long long int nbr, int base, int *len)
+{
+	char	*hexa = "0123456789abcdef";
+
+	if (nbr < 0)
+	{
+		*len += write(1, "-", 1);
+		nbr *= -1;
+	}
+	if (nbr >= base)
+		putnbr((nbr / base), base, len);
+	*len += write(1, &hexa[nbr % base], 1);
+}
