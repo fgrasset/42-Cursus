3460fc31c4f4cd337c410c758278f58a3b1a04b0
Fabien Grasset <fabien.grasset.99@gmail.com>
2023-07-07 16:18:25 +0200

    finished ex02

---
+++ Cpp/cpp01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string &stringREF = str;
+
+	//printing addresses
+	std::cout << "Address of the string: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+	//printing values
+	std::cout << "Value of string: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
