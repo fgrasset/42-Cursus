7f12daaada9070e1bc5e7bbb521c6c4da188b438
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-03-01 16:00:55 +0100

    on the way

---
+++ inception/srcs/requirements/wordpress/wp_start.sh
@@ -0,0 +1,28 @@
+echo "----------Starting Wordpress----------"
+
+php-fpm7.3 -v
+
+while ! mariadb -u $MARIADB_USER --password=$MARIADB_PASS -h mariadb -P 3306 --silent; do
+	sleep 1
+	echo "Mariadb not ready"
+done
+
+mariadb -u $MARIADB_USER --password=$MARIADB_PASS -h mariadb -P 3306 -e "SHOW DATABASES;"
+
+if [-e /var/www/wordpress/wp.php]
+then echo "wp_config exists"
+else
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+
+	cd /var/www/wordpress
+	wp core download --allow-root
+
+	wp config create --dbname=$MARIADB_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --dbhost=$WP_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
+
+fi
+
+php-fpm7.3 -F
