cde0b0da858ba89152347b5694e32c1bc9357030
Fabien Grasset <fabien.grasset.99@gmail.com>
2024-03-07 17:27:19 +0100

    updates

--- inception/srcs/requirements/mariadb/launch_mariadb.sh
+++ inception/srcs/requirements/mariadb/launch_mariadb.sh
@@ -3,6 +3,9 @@
 # Ensure MariaDB is not already running (prevents conflicts)
 if ! pgrep -x "mysqld" > /dev/null
 then
+  chown -R mysql:mysql /var/lib/mysql;
+  chown -R mysql:mysql /run/mysqld;
+
   # Start MariaDB in secure initialization mode
   mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql &
   pid=$!
@@ -11,11 +14,11 @@ then
   sleep 5
 
   # Configure MariaDB (using your variables from Script 1)
-  mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-  mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-  mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-  mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%';"
-  mysql -e "FLUSH PRIVILEGES;"
+  mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+  mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+  mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+  mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%';"
+  mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
   # Kill the temporary initialization process
   kill "$pid"
